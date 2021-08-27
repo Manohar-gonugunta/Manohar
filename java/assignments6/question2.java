9c8e0f44885e01d77cf4580cfc313891751ab8ff
Manohar-gonugunta <88088020+Manohar-gonugunta@users.noreply.github.com>
2021-08-27 13:04:21 +0530

    Add files via upload

---
+++ java/assignments6/question2.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<String> product=new TreeSet<>();
+		product.add("Manohar");
+		product.add("bannu");
+		product.add("gonugunta");
+		product.add("ABCD");
+		product.add("xyz");
+		product.add("jkl");
+		
+		product.add("Manohar");
+		System.out.println(product);
+				
+
+	}
+
+}
